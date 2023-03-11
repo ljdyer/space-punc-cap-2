6c46a0f891245d7618e9b008fca18a92d83fa80e
Laurence Dyer <ljdyer@gmail.com>
2023-03-11 18:15:21 +0000

    update

---
+++ laurence/byt5-test/simplet5-train.py
@@ -0,0 +1,83 @@
+import argparse
+import pandas as pd
+import more_itertools
+import socket
+from sklearn.model_selection import train_test_split
+from simplet5 import SimpleT5
+
+if socket.gethostname() == 'Laurences-MacBook-Air.local':
+    train_path, test_path = 'ted_train.csv', 'ted_test.csv'
+else:
+    train_path, test_path = '/data/ldyer/ted_train.csv', '/data/ldyer/ted_test.csv'
+
+CHUNK_LENGTH_TARGET = 100
+SOURCE_MAX_TOKEN_LEN = 100 
+TARGET_MAX_TOKEN_LEN = 150
+
+parser = argparse.ArgumentParser(description='Train or evaluate SimpletT5 model for feature restoration')
+parser.add_argument('mode', metavar='m', type=str, choices=['train', 'evaluate'])
+parser.add_argument('num_docs_to_use', metavar='n', type=str)
+
+
+# ====================
+def train(num_docs_to_use):
+
+    print(f'Loading training data from {train_path}...')
+    train_df = load_and_prep_df(train_path, num_docs_to_use)
+    train_df, val_df = train_test_split(train_df, test_size=0.2)
+    print(f'Num samples: {len(train_df)} train, {len(val_df)} validation')
+    model = SimpleT5()
+    model.from_pretrained(model_type="byt5", model_name="google/byt5-small")
+    model.train(train_df=train_df,
+                eval_df=val_df, 
+                source_max_token_len=SOURCE_MAX_TOKEN_LEN, 
+                target_max_token_len=TARGET_MAX_TOKEN_LEN, 
+                batch_size=8, 
+                max_epochs=10,
+                use_gpu=True
+            )
+
+
+# ====================
+def load_and_prep_df(csv_path, num_docs_to_use):
+
+    all_cleaned = pd.read_csv(csv_path)['all_cleaned'].to_list()
+    if num_docs_to_use == 'all':
+        text = ' '.join(all_cleaned)
+    else:
+        num_docs_to_use = int(num_docs_to_use)
+        text = ' '.join(all_cleaned[num_docs_to_use])
+    target_text = chunked_text(text, CHUNK_LENGTH_TARGET)
+    source_text = [remove_formatting(s) for s in target_text]
+    return pd.DataFrame({
+        'source_text': pd.Series(source_text),
+        'target_text': pd.Series(target_text)
+    })
+
+
+# ====================
+def chunked_text(text, n):
+
+    return [''.join(chunk)
+            for chunk
+            in more_itertools.chunked(list(text), n)]
+
+
+# ====================
+def remove_formatting(string):
+
+    string = string.lower().replace(' ', '').replace('.', '').replace(',', '')
+    return string
+
+
+# ====================
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    if args.mode == 'train':
+        train(args.num_docs_to_use)
+    elif args.mode == 'evaluate':
+        print('Evaluate: not implemented yet.')
+    
+
+
